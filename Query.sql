790928c8cc5e77f0a63b7fd236de4baf9f196a23
Vinh Nguyen <vinhrau14011212@gmail.com>
2026-01-03 12:14:01 +0700

    Query.sql

--- Query.sql
+++ Query.sql
@@ -12,3 +12,16 @@ WHERE NGAY_GIAO_DICH >= '20200101'
         TRI_GIA * PHAN_TRAM_HOA_HONG / 100.0 > 50000000
         OR DU_NO > 30000000
       );
+
+
+
+SELECT TOP 10
+    MA_CUA_HANG,      -- Store ID
+    MA_NV_BAN,        -- Sales employee ID
+    MA_MAT_HANG,      -- Product ID
+    TRI_GIA           -- Transaction value
+FROM dbo.HoaDon
+WHERE MA_KH = 'MKH303217'
+  AND MA_CUA_HANG = 'STORE 1'
+  AND NGAY_DEN_HAN > DATEADD(MONTH, 12, NGAY_GIAO_DICH)   -- strictly > 12 months
+ORDER BY NGAY_GIAO_DICH DESC, MA_HOA_DON DESC;
